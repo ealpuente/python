5974cf3d281b6d29dd583041b0d8e061329dc9f8
ALPUENTE TORRES - ELIAS <33405257B@dival.es>
2020-09-18 09:25:18 +0200

    inicial

---
+++ kwranking/kwranking.py
@@ -0,0 +1,58 @@
+
+import os
+import time
+
+fichero = 'c:\\Users\\33405257B\\AppData\\Local\\Programs\\Python\\Python38\\keywords.txt'
+
+def carga_keywords():
+    try:
+        f = open(fichero, 'rt')
+        lista = []
+        for linea in f:
+            lista.append(linea.rstrip('\n'))
+        return lista
+    except FileNotFoundError:
+        print('No se encuentra el fichero')
+
+def muestra_keywords(keywords):
+    contador = 0
+    for kw in keywords:
+        print(kw)
+        contador += 1
+        if contador == 20:
+            contador = 0
+            input('Mostrar más...')
+    input('Mostrar más...')
+    
+def mostrarMenu():
+
+    l = []
+
+    while True:
+        os.system ("cls") 
+
+        # Msotramos el menú
+        print("[1] - Importar palabras clave")
+        print("[2] - Mostrar palabras clave")
+        print("[0] - Salir")
+
+        entrada = input('Elige opción: ')    
+        try:
+            entrada = int(entrada)
+            if (entrada in range(0,3)):
+                
+                if (entrada == 1):
+                    l = carga_keywords()
+                    continue
+                elif (entrada == 2):
+                    muestra_keywords(l)
+                    continue
+                else:
+                    break
+            else:
+                continue
+        except ValueError:
+            continue
+
+
+mostrarMenu()
