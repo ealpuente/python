0c967997e0e47da65457d903943ea0a55cb50038
ealpuente <elias.alpuente@gmail.com>
2021-07-10 20:04:12 +0200

    python básico 1,2

---
+++ curso_django/3_Python_Basico/01_variables.py
@@ -0,0 +1,10 @@
+
+# Uso de variables
+name 		= "Andrés"
+name_person = "Andés"
+namePerson 	= "Andrés"
+
+edad 		= 10
+
+peso 		= 93.2
+
---
+++ curso_django/3_Python_Basico/02_tipos_datos.py
@@ -0,0 +1,29 @@
+
+#tipos de dato cadena
+cadena = "python"
+cadena2 = 'python con cadenas simples'
+
+print(cadena)
+print(cadena2)
+
+#tipo de datos entero
+entero = 55
+print(entero)
+
+#transformar entero en cadena
+print(cadena + " " + str(entero))
+
+#tipo de dato flotante
+flotante = 55.328457983274958723948579238475928374958723948759823745982734
+
+# convertir flotante a cadena y entero
+print(flotante)
+print(cadena + " " + str(flotante))
+print(int(flotante))
+
+# tipo de dato booleano
+verdad = True
+print(verdad)
+
+# averiguar el tipo de datos de una variable
+print(type(flotante))
