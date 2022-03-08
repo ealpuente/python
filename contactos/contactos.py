2bbfb8128f586416ad657bfd30af1cfc4a2c6010
ealpuente <elias.alpuente@gmail.com>
2022-03-08 20:22:06 +0100

    contactos

--- contactos/contactos.py
+++ contactos/contactos.py
@@ -1,7 +1,50 @@
-from flask import Flask
+
+import sqlite3
+from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
+''' MODELO '''
+def get_db_connection():
+	conn = None
+	try:
+		conn = sqlite3.connect('database.db')
+		print(sqlite3.version)
+	except Error as e:
+		print(e)
+		
+	return conn
+
+def get_db_entidad(id_entidad):
+	conn = get_db_connection()
+	entidad = conn.execute('SELECT * FROM entidad WHERE ID = ?', (id_entidad)).fetchall()
+	conn.close()
+
+	if entidad == None:
+		abort('404')
+
+	return entidad
+
+def get_db_lista_entidades():
+	lista = None
+	conn = get_db_connection()
+	if conn != None:
+		lista = conn.execute('SELECT * FROM entidad').fetchall()
+
+	if lista == None:
+		abort('404')
+
+	return lista
+
+
+''' CONTROLADOR '''
 @app.route("/")
 def inicio():
-	return "Pagina inicio"
+	lista = get_db_lista_entidades()
+	return render_template("index.html", entidades = lista)
+
+@app.route("/detalle/<string:id_entidad>")
+def detalle(id_entidad):
+	entidad = get_db_entidad(id_entidad)
+	return render_template("detalle.html", entidad=entidad[0])
---
+++ contactos/init.sh
@@ -0,0 +1,3 @@
+source ./env/bin/activate
+export FLASK_APP=contactos
+export FLASK_ENV=development
