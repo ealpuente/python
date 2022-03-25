cdd7404c12b029dfed588d8b9c8f259330503830
Elias Alpuente <elias.alpuente@dival.com>
2022-03-25 12:55:21 +0100

    Ver posts individuales

--- flaskblog/app.py
+++ flaskblog/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 import sqlite3
 
 app = Flask(__name__)
@@ -8,10 +9,25 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM POSTS WHERE ID = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post == None:
+        abort(404)
+
+
 @app.route("/")
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template("index.html", posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
 
