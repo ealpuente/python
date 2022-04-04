6c933a7a0d93b1076398bdeb46d6fcb822113046
ealpuente <elias.alpuente@gmail.com>
2022-04-04 20:59:45 +0200

    flaskblog

--- flaskblog/app.py
+++ flaskblog/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
 from werkzeug.exceptions import abort
 import sqlite3
+import json
 
 
 app = Flask(__name__)
@@ -80,3 +81,77 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+
+'''***********************'''
+'''**  DEFINICION API   **'''
+'''***********************'''
+
+def wrapAPI(result, data):
+	return {"result":"000", "data":data}
+
+@app.route("/api/v1/list")
+def indexAPI():
+	conn = get_db_connection()
+	posts = conn.execute('SELECT * FROM posts').fetchall()
+	conn.close()
+
+	return jsonify(wrapAPI('000', [dict(row) for row in posts]))
+
+@app.route('<int:post_id>')
+def postAPI(post_id):
+	post = get_post(post_id)
+
+	return jsonify(wrapAPI('000', [dict(row) for row in post]))
+
+@app.route('/api/v1/create', methods=('POST',))
+def createAPI():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+@app.route('/api/v1/<int:post_id>/edit', methods=('POST',))
+def editAPI(post_id):
+    post = get_post(post_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/api/v1/<int:id>/delete', methods=('POST',))
+def deleteAPI(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+
+
+
+
---
+++ flaskblog/initdev.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=app 
+export FLASK_DEBUG=true
+export FLASK_ENV=development
+flask run
