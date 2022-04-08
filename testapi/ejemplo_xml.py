a92c8972dd82b6d7f8b6a485cc24542a60b03722
Elias Alpuente <elias.alpuente@dival.com>
2022-04-08 13:51:47 +0200

    manejo xml

--- testapi/ejemplo_xml.py
+++ testapi/ejemplo_xml.py
@@ -1,6 +1,10 @@
 
 import xml.etree.ElementTree as ET
 
+'''
+Ejemplo de uso del tratamiento de un xml que se obtiene como respuesta a la llamada de un servicio web
+
+'''
 
 xml_ejemplo = '''<?xml version="1.0"?>
 <data>
@@ -26,20 +30,45 @@ xml_ejemplo = '''<?xml version="1.0"?>
     </country>
 </data>'''
 
+# Parsear el archivo y obtener el nodo raiz
 root = ET.fromstring(xml_ejemplo)
+print("*********** NODO RAIZ **********")
 print(root.tag)
 print(root.attrib)
 
+# Acceder a los nodos hijos
+print("*********** ACCESO A LOS NODOS HIJOS **********")
 for child in root:
   print(child.tag, child.attrib)
+  for subchild in child:
+    print("\t", subchild.tag, subchild.attrib, subchild.text)
+  print("\n")
 
-print(root[0][2].text)
+# Acceso a los hijos mediante listas
+print("*********** ACCESO A LOS NODOS HIJOS: LISTAS **********")
+print(root[0][0].tag, ": ", root[0][0].text)
+for c in range(len(root)):
+  print(root[c].tag, root[c].attrib)
 
+print("*********** ITERAR POR EL ARBOL: ITER **********")
+# Recorre todo el arbol buscando nodos con tag especificado e imprime sus atributos (direccionario)
 for neighbor in root.iter('neighbor'):
   print(neighbor.attrib)
+  print(neighbor.attrib['name'])
+
+print("*********** ITERAR POR EL ARBOL: FINDALL **********")
+for country in root.findall('country'):
+  rank = country.find('rank').text
+  name = country.get('name')
+  print(name, rank)
+
+'''
+
+
 
 for country in root.findall('country'):
   rank = country.find('rank').text
   name = country.get('name')
   print(name, rank)
 
+'''
